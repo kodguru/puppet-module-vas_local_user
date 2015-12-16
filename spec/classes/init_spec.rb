8afacf723bb4acff222f28f39b208eda126a36cc
Anders Larsson <anders.x.larsson@ericsson.com>
2015-12-16 12:50:04 +0100

    Refactor spec tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 describe 'vas_local_user' do
 
+  let(:validation_facts) { {
+  }}
+
   let :facts do
     {
       :kernel                     => 'Linux',
@@ -33,19 +36,17 @@ describe 'vas_local_user' do
           'class vas { }
           include vas'
       }
+      let (:facts) { validation_facts.merge({:fqdn => 'vasenabled.example.local'}) }
 
-      it 'should contain user user1' do
-        facts.merge!({'fqdn' => 'vasenabled.example.local'})
-        should contain_user('user1').with({
+      it { should contain_user('user1').with({
           'ensure'    => 'present',
           'uid'         => '1001',
           'gid'         => '100',
           'home'        => '/var/lib/user1',
           'managehome'  => 'true',
           'forcelocal'  => 'true',
-        })
-        should contain_user('user1').that_comes_before('Class[vas]')
-      end
+        }) }
+      it { should contain_user('user1').that_comes_before('Class[vas]') }
     end
 
     context 'on system with VAS managed and installed' do
@@ -53,21 +54,17 @@ describe 'vas_local_user' do
           'class vas { }
           include vas'
       }
+      let (:facts) { validation_facts.merge({:fqdn => 'vasenabled.example.local',
+                                             :vas_version => '4.1.0.21518'}) }
 
-      it 'should not contain user1' do
-        facts.merge!({'vas_version' => '4.1.0.21518',
-                      'fqdn' => 'vasenabled.example.local'})
-        should_not contain_user('user1')
-      end
+      it { should_not contain_user('user1') }
     end
 
     context 'on system without VAS managed but installed' do
+      let (:facts) { validation_facts.merge({:fqdn => 'vasenabled.example.local',
+                                             :vas_version => '4.1.0.21518'}) }
 
-      it 'should not contain user1' do
-        facts.merge!({'vas_version' => '4.1.0.21518',
-                      'fqdn' => 'vasenabled.example.local'})
-        should_not contain_user('user1')
-      end
+      it { should_not contain_user('user1') }
     end
 
     context 'on system without VAS' do
@@ -111,31 +108,18 @@ describe 'vas_local_user' do
           'class vas { }
           include vas'
       }
+      let (:facts) { validation_facts.merge({:fqdn => 'vasenabled.example.local'}) }
 
-      it 'should contain user user1' do
-        facts.merge!({'fqdn' => 'vasenabled.example.local'})
-        should contain_user('user1')
-      end
-      it 'should contain user1\'s ssh_authorized_keys' do
-        facts.merge!({'fqdn' => 'vasenabled.example.local'})
-        should contain_ssh_authorized_key('user1')
-      end
+      it { should contain_user('user1') }
+      it { should contain_ssh_authorized_key('user1') }
     end
 
     context 'on system without VAS managed but installed' do
+      let (:facts) { validation_facts.merge({:fqdn => 'vasenabled.example.local',
+                                             :vas_version => '4.1.0.21518'}) }
 
-      it 'should not contain user1' do
-        facts.merge!({'vas_version' => '4.1.0.21518',
-                      'fqdn' => 'vasenabled.example.local'})
-        should_not contain_user('user1')
-      end
-
-      it 'should not contain user1\'s ssh_authorized_keys' do
-        facts.merge!({'vas_version' => '4.1.0.21518',
-                      'fqdn' => 'vasenabled.example.local'})
-        should_not contain_ssh_authorized_key('user1')
-      end
-
+      it { should_not contain_user('user1') }
+      it { should_not contain_ssh_authorized_key('user1') }
     end
 
     context 'on system with VAS managed and installed' do
@@ -143,27 +127,19 @@ describe 'vas_local_user' do
           'class vas { }
           include vas'
       }
+      let (:facts) { validation_facts.merge({:fqdn => 'vasenabled.example.local',
+                                             :vas_version => '4.1.0.21518'}) }
 
-      it 'should not contain user1' do
-        facts.merge!({'vas_version' => '4.1.0.21518',
-                      'fqdn' => 'vasenabled.example.local'})
-        should_not contain_user('user1')
-      end
-
-      it 'should not contain user1\'s ssh_authorized_keys' do
-        facts.merge!({'vas_version' => '4.1.0.21518',
-                      'fqdn' => 'vasenabled.example.local'})
-        should_not contain_ssh_authorized_key('user1')
-      end
-
+      it { should_not contain_user('user1') }
+      it { should_not contain_ssh_authorized_key('user1') }
     end
 
     context 'on system without VAS' do
 
       it { should contain_user('user1') }
       it { should contain_ssh_authorized_key('user1') }
-
     end
+
   end
 
   describe 'hiera merge' do
@@ -175,28 +151,13 @@ describe 'vas_local_user' do
             'class vas { }
             include vas'
         }
+        let (:facts) { validation_facts.merge({:fqdn => 'vasenabled-user.example.local',
+                                               :spectest => 'user1'}) }
 
-        it 'should contain user user1' do
-          facts.merge!({'fqdn'      => 'vasenabled-user.example.local',
-                        'spectest'  => 'user1'})
-          should contain_user('user1')
-        end
-        it 'should contain user user2' do
-          facts.merge!({'fqdn'      => 'vasenabled-user.example.local',
-                        'spectest'  => 'user1'})
-          should contain_user('user2')
-        end
-        it 'should contain ssh_authorized_keys for user1' do
-          facts.merge!({'fqdn'      => 'vasenabled-user.example.local',
-                        'spectest'  => 'user1'})
-          should contain_ssh_authorized_key('user1')
-        end
-        it 'should not contain ssh_authorized_keys for user2' do
-          facts.merge!({'fqdn'      => 'vasenabled-user.example.local',
-                        'spectest'  => 'user1'})
-          should_not contain_ssh_authorized_key('user2')
-        end
-
+        it { should contain_user('user1') }
+        it { should contain_user('user2') }
+        it { should contain_ssh_authorized_key('user1') }
+        it { should_not contain_ssh_authorized_key('user2') }
       end
 
       context 'on system with VAS managed and installed' do
@@ -204,83 +165,33 @@ describe 'vas_local_user' do
             'class vas { }
             include vas'
         }
-
-        it 'should not contain user user1' do
-          facts.merge!({'vas_version' => '4.1.0.21518',
-                        'fqdn' => 'vasenabled-user.example.local',
-                        'spectest'  => 'user1'})
-          should_not contain_user('user1')
-        end
-        it 'should not contain user user2' do
-          facts.merge!({'vas_version' => '4.1.0.21518',
-                        'fqdn' => 'vasenabled-user.example.local',
-                        'spectest'  => 'user1'})
-          should_not contain_user('user2')
-        end
-        it 'should not contain ssh_authorized_keys for user2' do
-          facts.merge!({'vas_version' => '4.1.0.21518',
-                        'fqdn' => 'vasenabled-user.example.local',
-                        'spectest'  => 'user1'})
-          should_not contain_ssh_authorized_key('user1')
-        end
-        it 'should not contain ssh_authorized_keys for user2' do
-          facts.merge!({'fqdn'      => 'vasenabled-user.example.local',
-                        'spectest'  => 'user1'})
-          should_not contain_ssh_authorized_key('user2')
-        end
-
+        let (:facts) { validation_facts.merge({:fqdn => 'vasenabled-user.example.local',
+                                               :spectest => 'user1',
+                                               :vas_version => '4.1.0.21518'}) }
+        it { should_not contain_user('user1') }
+        it { should_not contain_user('user2') }
+        it { should_not contain_ssh_authorized_key('user1') }
+        it { should_not contain_ssh_authorized_key('user2') }
       end
 
       context 'on system without VAS managed but installed' do
+        let (:facts) { validation_facts.merge({:fqdn => 'vasenabled-user.example.local',
+                                               :spectest => 'user1',
+                                               :vas_version => '4.1.0.21518'}) }
 
-        it 'should not contain user user1' do
-          facts.merge!({'vas_version' => '4.1.0.21518',
-                        'fqdn' => 'vasenabled-user.example.local',
-                        'spectest'  => 'user1'})
-          should_not contain_user('user1')
-        end
-        it 'should not contain user user2' do
-          facts.merge!({'vas_version' => '4.1.0.21518',
-                        'fqdn' => 'vasenabled-user.example.local',
-                        'spectest'  => 'user1'})
-          should_not contain_user('user2')
-        end
-        it 'should not contain ssh_authorized_keys for user2' do
-          facts.merge!({'vas_version' => '4.1.0.21518',
-                        'fqdn' => 'vasenabled-user.example.local',
-                        'spectest'  => 'user1'})
-          should_not contain_ssh_authorized_key('user1')
-        end
-        it 'should not contain ssh_authorized_keys for user2' do
-          facts.merge!({'fqdn'      => 'vasenabled-user.example.local',
-                        'spectest'  => 'user1'})
-          should_not contain_ssh_authorized_key('user2')
-        end
-
+        it { should_not contain_user('user1') }
+        it { should_not contain_user('user2') }
+        it { should_not contain_ssh_authorized_key('user1') }
+        it { should_not contain_ssh_authorized_key('user2') }
       end
 
       context 'on system without VAS' do
-        it 'should contain user user1' do
-          facts.merge!({'fqdn'      => 'vasdisabled-user.example.local',
-                        'spectest'  => 'user1'})
-          should contain_user('user1')
-        end
-        it 'should contain user user2' do
-          facts.merge!({'fqdn'      => 'vasdisabled-user.example.local',
-                        'spectest'  => 'user1'})
-          should contain_user('user2')
-        end
-        it 'should contain ssh_authorized_keys for user1' do
-          facts.merge!({'fqdn'      => 'vasdisabled-user.example.local',
-                        'spectest'  => 'user1'})
-          should contain_ssh_authorized_key('user1')
-        end
-        it 'should not contain ssh_authorized_keys for user2' do
-          facts.merge!({'fqdn'      => 'vasdisabled-user.example.local',
-                        'spectest'  => 'user1'})
-          should_not contain_ssh_authorized_key('user2')
-        end
-
+        let (:facts) { validation_facts.merge({:fqdn => 'vasdisabled-user.example.local',
+                                               :spectest => 'user1'}) }
+        it { should contain_user('user1') }
+        it { should contain_user('user2') }
+        it { should contain_ssh_authorized_key('user1') }
+        it { should_not contain_ssh_authorized_key('user2') }
       end
 
     end
@@ -292,27 +203,13 @@ describe 'vas_local_user' do
             'class vas { }
             include vas'
         }
+        let (:facts) { validation_facts.merge({:fqdn => 'vasenabled-userkey.example.local',
+                                               :spectest => 'user1'}) }
 
-        it 'should contain user user1' do
-          facts.merge!({'fqdn'      => 'vasenabled-userkey.example.local',
-                        'spectest'  => 'user1'})
-          should contain_user('user1')
-        end
-        it 'should contain user user2' do
-          facts.merge!({'fqdn'      => 'vasenabled-userkey.example.local',
-                        'spectest'  => 'user1'})
-          should contain_user('user2')
-        end
-        it 'should contain ssh_authorized_keys for user1' do
-          facts.merge!({'fqdn'      => 'vasenabled-userkey.example.local',
-                        'spectest'  => 'user1'})
-          should contain_ssh_authorized_key('user1')
-        end
-        it 'should contain ssh_authorized_keys for user2' do
-          facts.merge!({'fqdn'      => 'vasenabled-userkey.example.local',
-                        'spectest'  => 'user1'})
-          should contain_ssh_authorized_key('user2')
-        end
+        it { should contain_user('user1') }
+        it { should contain_user('user2') }
+        it { should contain_ssh_authorized_key('user1') }
+        it { should contain_ssh_authorized_key('user2') }
       end
 
       context 'on system with VAS managed and installed' do
@@ -320,83 +217,34 @@ describe 'vas_local_user' do
             'class vas { }
             include vas'
         }
+        let (:facts) { validation_facts.merge({:fqdn => 'vasenabled-userkey.example.local',
+                                               :spectest => 'user1',
+                                               :vas_version => '4.1.0.21518'}) }
 
-        it 'should not contain user user1' do
-          facts.merge!({'vas_version' => '4.1.0.21518',
-                        'fqdn'        => 'vasenabled-userkey.example.local',
-                        'spectest'    => 'user1'})
-          should_not contain_user('user1')
-        end
-        it 'should not contain user user2' do
-          facts.merge!({'vas_version' => '4.1.0.21518',
-                        'fqdn'        => 'vasenabled-userkey.example.local',
-                        'spectest'    => 'user1'})
-          should_not contain_user('user2')
-        end
-        it 'should not contain ssh_authorized_keys for user1' do
-          facts.merge!({'vas_version' => '4.1.0.21518',
-                        'fqdn'        => 'vasenabled-userkey.example.local',
-                        'spectest'    => 'user1'})
-          should_not contain_ssh_authorized_key('user1')
-        end
-        it 'should not contain ssh_authorized_keys for user2' do
-          facts.merge!({'vas_version' => '4.1.0.21518',
-                        'fqdn'        => 'vasenabled-userkey.example.local',
-                        'spectest'    => 'user1'})
-          should_not contain_ssh_authorized_key('user2')
-        end
+        it { should_not contain_user('user1') }
+        it { should_not contain_user('user2') }
+        it { should_not contain_ssh_authorized_key('user1') }
+        it { should_not contain_ssh_authorized_key('user2') }
       end
 
       context 'on system without VAS managed but installed' do
+        let (:facts) { validation_facts.merge({:fqdn => 'vasenabled-userkey.example.local',
+                                               :spectest => 'user1',
+                                               :vas_version => '4.1.0.21518'}) }
 
-        it 'should not contain user user1' do
-          facts.merge!({'vas_version' => '4.1.0.21518',
-                        'fqdn'        => 'vasenabled-userkey.example.local',
-                        'spectest'    => 'user1'})
-          should_not contain_user('user1')
-        end
-        it 'should not contain user user2' do
-          facts.merge!({'vas_version' => '4.1.0.21518',
-                        'fqdn'        => 'vasenabled-userkey.example.local',
-                        'spectest'    => 'user1'})
-          should_not contain_user('user2')
-        end
-        it 'should not contain ssh_authorized_keys for user1' do
-          facts.merge!({'vas_version' => '4.1.0.21518',
-                        'fqdn'        => 'vasenabled-userkey.example.local',
-                        'spectest'    => 'user1'})
-          should_not contain_ssh_authorized_key('user1')
-        end
-        it 'should not contain ssh_authorized_keys for user2' do
-          facts.merge!({'vas_version' => '4.1.0.21518',
-                        'fqdn'        => 'vasenabled-userkey.example.local',
-                        'spectest'    => 'user1'})
-          should_not contain_ssh_authorized_key('user2')
-        end
+        it { should_not contain_user('user1') }
+        it { should_not contain_user('user2') }
+        it { should_not contain_ssh_authorized_key('user1') }
+        it { should_not contain_ssh_authorized_key('user2') }
       end
 
       context 'on system without VAS' do
-        it 'should contain user user1' do
-          facts.merge!({'fqdn'      => 'vasdisabled-userkey.example.local',
-                        'spectest'  => 'user1'})
-          should contain_user('user1')
-        end
-        it 'should contain user user2' do
-          facts.merge!({'fqdn'      => 'vasdisabled-userkey.example.local',
-                        'spectest'  => 'user1'})
-          should contain_user('user2')
-        end
-        it 'should contain ssh_authorized_keys for user1' do
-          facts.merge!({'fqdn'      => 'vasdisabled-userkey.example.local',
-                        'spectest'  => 'user1'})
-          should contain_ssh_authorized_key('user1')
-        end
-        it 'should contain ssh_authorized_keys for user2' do
-          facts.merge!({'fqdn'      => 'vasdisabled-userkey.example.local',
-                        'spectest'  => 'user1'})
-          should contain_ssh_authorized_key('user2')
-        end
-
+        let (:facts) { validation_facts.merge({:fqdn => 'vasdisabled-userkey.example.local',
+                                               :spectest => 'user1'}) }
+        it { should contain_user('user1') }
+        it { should contain_user('user2') }
+        it { should contain_ssh_authorized_key('user1') }
+        it { should contain_ssh_authorized_key('user2') }
       end
     end
   end
