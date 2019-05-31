a86836d3047966b7b9bdc258a103eb9a522533b0
Anders Larsson <anders.x.larsson@ericsson.com>
2019-05-31 07:25:40 +0200

    Remove config from fact vas_local_user_libuser
    Not always installed by package and is not necessary for libuser to
    work.

--- lib/facter/vas_local_user_libuser.rb
+++ lib/facter/vas_local_user_libuser.rb
@@ -1,8 +1,7 @@
 Facter.add('vas_local_user_libuser') do
   setcode do
     if File.executable?('/usr/sbin/luseradd') &&
-       File.executable?('/usr/sbin/lgroupadd') &&
-       File.exist?('/etc/libuser.conf')
+       File.executable?('/usr/sbin/lgroupadd')
       'yes'
     else
       'no'
